c64cde1302810c74945b875f908b37851c3f0dfd
Eduariag <uariagsmart@gmail.com>
2022-12-02 19:13:39 +0300

    решил 3 задачи

--- Program.cs
+++ Program.cs
@@ -44,11 +44,11 @@ Console.WriteLine(max);
  //    Задача 6: Напишите программу, которая на вход принимает число
  // и выдаёт, является ли число чётным (делится ли оно на два без остатка).   
     
-  
+  /*
 Console.Write("Input a number:  ");
 int num=Convert.ToInt32(Console.ReadLine());
 
-if(int num/2)
+if(num % 2 ==0)
 {
     Console.WriteLine("Yes");
 }    
@@ -56,3 +56,4 @@ else
 {
     Console.WriteLine("No");
 }
+*/
