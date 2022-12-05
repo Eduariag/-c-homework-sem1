fdde1237d8c87021967f83a517de096cb19bb62a
Eduariag <uariagsmart@gmail.com>
2022-12-05 21:29:16 +0300

    все задачи

--- Program.cs
+++ Program.cs
@@ -61,13 +61,20 @@ else
 //Задача 8: Напишите программу, которая на вход принимает число (N),
 // а на выходе показывает все чётные числа от 1 до N.
 
+/*
 Console.Write("Input an integre number:  ");
 int num=Convert.ToInt32(Console.ReadLine());
 
 int current=num;
 
 while(current=2%0)
+if(current<=1)
 {
     Console.Write(current + "  ");
     current++;
 }
+else
+{
+    current++;
+}
+*/
